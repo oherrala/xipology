8e67eaeb0d46dfa2c8cf63bcb6fa5d031cb2104f
Ossi Herrala <oherrala@iki.fi>
2017-10-17 22:21:01 +0300

    Latest advancements in Rendezvous technology

--- rendezvous/main.rs
+++ rendezvous/main.rs
@@ -9,8 +9,13 @@ extern crate trust_dns;
 
 extern crate xipolib;
 
+use std::collections::HashSet;
+use std::ops::Div;
+use std::str;
 use std::thread;
+
 use chrono::prelude::*;
+use rand::{Rng, OsRng};
 
 use xipolib::ReadError;
 
@@ -44,57 +49,100 @@ fn main() {
     let mut xipo = xipolib::Xipology::from_secret(server, From::from(base_secret));
     let mut secret = SecretGen::from(base_secret);
 
+    let mut rng = OsRng::new().expect("OsRng::new");
+    let mut encounters: HashSet<String> = HashSet::new();
+
     loop {
         let next = secret.secret();
         println!(
-            "Using secret {}",
+            "Reading rendezvous point {}",
             std::str::from_utf8(&next).expect("from_utf8")
         );
         xipo.change_secret(next);
 
         match xipo.read_bytes() {
-            Ok(found_nick) => {
-                let as_str = std::str::from_utf8(&found_nick).expect("from_utf8");
-                println!("Found nick: {}", as_str);
+            Ok(found_nicks) => {
+                if let Ok(as_str) = std::str::from_utf8(&found_nicks) {
+                    eprintln!("Read something: {}", as_str);
+                    encounters.extend(decode_nicks(as_str.as_bytes()));
+                }
                 continue;
             }
             Err(ReadError::Free) => {
                 println!("Rendezvous point was free. Will take the next one");
-                let next = secret.secret();
-                xipo.change_secret(next);
-                let _ = xipo.write_bytes(nick.as_bytes());
-                println!("Sleeping for 30 seconds");
-                thread::sleep(std::time::Duration::from_secs(30));
+
+                // Write back all our encountered nicks and our own
+                encounters.insert(nick.clone());
+                xipo.change_secret(secret.secret());
+                let nicks = encode_nicks(&encounters);
+                let _ = xipo.write_bytes(&nicks);
+                eprintln!("Scribbled on ether: {:?}", encounters);
+                let sleep = rng.gen_range(55, 65);
+                println!("Sleeping for {} seconds", sleep);
+                thread::sleep(std::time::Duration::from_secs(sleep));
             }
             Err(ReadError::Consumed) => {
-                println!("Rendezvous point was already consumed");
+                println!("Rendezvous point was already consumed. Moving to next one.");
                 continue;
             }
             Err(err) => {
                 eprintln!("ERROR: Read error: {:?}", err);
             }
         }
+
+        encounters.clear();
     }
 }
 
 struct SecretGen<'a> {
     secret: &'a str,
+    used_time: String,
     counter: usize,
 }
 
 impl<'a> SecretGen<'a> {
     pub fn from(secret: &'a str) -> Self {
+        let time = Self::get_time();
         Self {
             secret: secret,
+            used_time: time,
             counter: 0,
         }
     }
 
     pub fn secret(self: &mut Self) -> Vec<u8> {
-        let utc: DateTime<Utc> = Utc::now();
-        let time = utc.format("%FT%R"); // YYYY-mm-DDTHH-MM
+        let time = Self::get_time();
+        if time != self.used_time {
+            self.counter = 0;
+            self.used_time = time.clone();
+        }
         let secret = format!("{}-{}-{}", self.secret, time, self.counter);
         self.counter += 1;
         secret.into_bytes()
     }
+
+    pub fn get_time() -> String {
+        // Seconds since UNIX epoch divided by n minutes
+        let epoch = Utc::now().timestamp();
+        let n = 5;
+        let time = epoch.div(n * 60);
+        format!("{}", time)
+    }
+}
+
+
+fn encode_nicks(nicks: &HashSet<String>) -> Vec<u8> {
+    let nicks: String = nicks.iter().cloned().collect::<Vec<_>>().join(", ");
+    nicks.into_bytes()
+}
+
+/// ```rust
+/// let nicks = "foo, bar, baz";
+/// let decoded = decode_nicks(&nicks).expect("decode_nicks");
+/// let encoded = encode_nicks(decoded);
+/// assert_eq!(nicks, encoded);
+/// ```
+fn decode_nicks(buf: &[u8]) -> HashSet<String> {
+    let as_str = str::from_utf8(buf).expect("from_utf8");
+    as_str.split(",").map(str::trim).map(From::from).collect()
 }
